0f0b9ddaeba5332f5b5433364ebff22d1bcf45f6
JulianevonHagen <julianevonhagen@hotmail.com>
2022-01-08 21:23:16 +0100

    changes on header, centered the logo

--- WD/giving/src/components/logo/Logo.js
+++ WD/giving/src/components/logo/Logo.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import './Logo.css'
-import {ReactComponent as ReactLogo} from './pictures/Giving-svg.svg'
+import React from 'react';
+import './Logo.css';
+import {ReactComponent as ReactLogo} from './pictures/Giving-svg.svg';
 
 const Logo = () => {
     return (
         <div className='Logo'>
-            < ReactLogo />
+            <ReactLogo />
         </div>
     );
 };
