5b513081e740c49e37197e587c9b8679bb991714
doddy666 <audrey.biasucci@gmail.com>
2022-01-15 18:49:08 +0100

    searchPage and Explore page

--- WD/giving/src/pages/ExplorePage/ExplorePage.js
+++ WD/giving/src/pages/ExplorePage/ExplorePage.js
@@ -1,5 +1,29 @@
-import { ReactFragment } from 'react';
-import Logo from ''
-import Button1 from '../../components/button1/Button1';
+import React from 'react';
+import Header from '../../components/header/header';
 import { Link } from 'react-router-dom';
-import search
+import Search from '../../components/Search/search'
+
+const Explore = () => {
+    return (
+        <div className='ExplorePage'>
+            <Link to={`/signup`}>
+                <Header />
+                <Search />
+            </Link>
+            < br />
+            <h3>Results</h3>
+            
+            return (
+            <div>
+                <ul>
+                    {organisation.map((name) => (
+                        <li key={name.id}>{organisation.name}</li>
+                    ))}
+                </ul>
+            </div>
+            );
+        </div>
+    );
+}     
+
+export default Explore
