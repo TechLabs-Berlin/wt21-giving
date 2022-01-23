1665e4c1e2ccdba935778b1d58a14f80e060fcb1
doddy666 <audrey.biasucci@gmail.com>
2022-01-23 14:24:30 +0100

    ThankYoupage Updates

--- WD/giving/src/pages/ThankYou/ThankYou.js
+++ WD/giving/src/pages/ThankYou/ThankYou.js
@@ -4,19 +4,32 @@ import Logo from '../../components/logo/Logo';
 import Button from '../../components/button/Button';
 import '/FotoOrganisation.js';
 import SocialFollow from '../../components/SocialFollow/SocialFollow'
-
+import Textfield3 from '../../components/textfield3/Textfield3'
 
 const ThankYou = () => {
     return (
         <div>
             <Logo />
             <Header />
+            
             <div className='Thanks'>
             <h1>THANK YOU</h1>
             <img className='LogoOrganisation' alt='Logo Organisation'/>
-                </div>
+            </div>
+            <Link to={`/Organisation`}>
+                <Button text='Back to Organisation Page'/>
+            </Link>
             <Button />
+            
+            <div className='Share'>
             <SocialFollow />
+            </div>
+            <div className='Rally'>
+                <p>Or rally this donation further by sending this link:</p>
+                <Textfield3 text='https://givingapp.com/donations...' />
+                <Button text='Rally now'/>
+           </div>
+
         </div>
     );
 };
