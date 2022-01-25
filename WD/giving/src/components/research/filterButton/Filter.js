4b60e602c8c082e51372de39161fee7dfa520dd0
doddy666 <audrey.biasucci@gmail.com>
2022-01-26 00:25:09 +0100

    FilterButton

---
+++ WD/giving/src/components/research/filterButton/Filter.js
@@ -0,0 +1,21 @@
+import React from "react";
+<<<<<<< Updated upstream:WD/giving/src/components/research/filter/Filter.js
+import { HiOutlineAdjustments } from 'react-icons'
+=======
+import { HiOutlineAdjustments } from 'react-icons/hi'
+import './FilterButton.css'
+>>>>>>> Stashed changes:WD/giving/src/components/research/filterButton/Filter.js
+
+const filter = () => {
+
+    return (
+        <div id='adjustment'>
+            <div className='adjustment'>
+                <HiOutlineAdjustments size="30px"
+                />
+            </div> 
+            </div>
+    );
+};
+
+export default filter;
