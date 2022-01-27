d5d5febcd9a31dd7589905d336f8db1e2646687a
doddy666 <audrey.biasucci@gmail.com>
2022-01-27 18:56:54 +0100

    FilterPage

--- WD/giving/src/app.js
+++ WD/giving/src/app.js
@@ -4,6 +4,8 @@ import Signup from './pages/signup/signup';
 import Payment from './pages/payment/payment';
 import Intro from './pages/intro/intro';
 import Login from './pages/login/login';
+import ExplorePage from './pages/ExplorePage/ExplorePage';
+import FilterPage from './pages/FilterPage/FilterPage'
 
 const app = () => {
     return (
@@ -12,7 +14,9 @@ const app = () => {
             <Route path="/" element={<Intro />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/payment" element={<Payment />} />
-            <Route path="/login" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/explorePage" element={<ExplorePage />} />
+                <Route path="/filterPage" element={<FilterPage />} />
         </Routes>
         </BrowserRouter>
     );
