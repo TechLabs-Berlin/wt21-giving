51b346624f12db14d16ceb4dd0d5b0e5d770c424
doddy666 <audrey.biasucci@gmail.com>
2022-01-28 22:22:16 +0100

    OrganisationPage

--- WD/giving/src/pages/Organisation/Organisation.js
+++ WD/giving/src/pages/Organisation/Organisation.js
@@ -1,31 +1,37 @@
 import React from 'react';
-import Header from '../../components/header/header'
-import Logo from '../../components/logo/Logo';
 import Button from '../../components/button/Button';
 import '/FotoOrganisation.js';
 import OrganisationInfo from '../components/OrganisationInfo/OrganisationInfo';
-import OrganisationName from '../../components/OrganisationInfo/NameOrganisation'
+import Header from '../../components/header/header'
+import OrganisationName from '../../components/OrganisationInfo/NameOrganisation/OrganisationName'
+import { Link } from 'react-router-dom';
+import SocialFollow from '../../components/SocialFollow/SocialFollow'
 
 const organisation = () => {
     return (
-        <div>
-            <Logo />
+        <div classeName='organisationpage'>
+            <div className='header'>
             <Header />
-            <div className='Logo'>
-                <img className='LogoOrganisation' alt='Logo Organisation' />
-                </div>
-            <div className='OrganisationName'>
-           <OrganisationName />
+            </div>
+            <br />
+            <img className='LogoOrganisation' alt='Logo Organisation'/>
+            <div className='organisation-name'>
+                <text src={OrganisationName} alt="organisation Name" />
                 <h2>Website</h2>
-                <div className='OrganisationInfo'>
+                <div className='organisationinfos'>
                 <OrganisationInfo />
                 </div>
-            <div className='Button'> 
-                    <Button text='Donate' />
-                    </div>  
             </div>
+<Link to={`/DonationPage`}>
+            <Button text='Donate'/>
+            </Link>
+            <div className='socialfollow'>
+                <SocialFollow />
             </div>
-       
+            <Link to={`/explorepage`}>
+            <Footer />
+            </Link>
+        </div>
     );
 };
 
